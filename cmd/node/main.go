6ded0af162965768946a158c19dea4956c392225
Icobart <enrico.cornacchia@studio.unibo.it>
2026-08-17 17:40:38 +0200

    feat: start TCP listener and register gRPC node server

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -5,8 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 
+	"google.golang.org/grpc"
+
+	"github.com/Icobart/ds-inventory-manager/internal/server"
 	"github.com/Icobart/ds-inventory-manager/internal/storage"
+	"github.com/Icobart/ds-inventory-manager/pb"
 	_ "modernc.org/sqlite"
 )
 
@@ -17,6 +22,7 @@ func main() {
 
 	log.Printf("Starting Warehouse Node: %s on port %d", *nodeID, *port)
 
+	// Starting the SQLite database
 	dbPath := fmt.Sprintf("%s.db", *nodeID)
 	db, err := sql.Open("sqlite", dbPath)
 	if err != nil {
@@ -28,4 +34,21 @@ func main() {
 		log.Fatalf("Failed to initialize database tables: %v", err)
 	}
 	log.Println("Database initialized successfully.")
+
+	// Starting the TCP listener
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen on port %d: %v", *port, err)
+	}
+
+	// Initializing and registering the gRPC server
+	grpcServer := grpc.NewServer()
+	nodeServer := server.NewNodeServer(*nodeID, db)
+	pb.RegisterInventoryManagerServer(grpcServer, nodeServer)
+	log.Printf("gRPC server actively listening at %v", lis.Addr())
+
+	// Start Serving to block the main thread, avoiding the program exiting
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC: %v", err)
+	}
 }
